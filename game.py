e1eb2670ada36f815e65a5e2d529159fa590ea5e
Alex40144 <giddyraccoon@gmail.com>
2020-06-24 17:18:00 +0100

    changed to be easier to read

--- game.py
+++ game.py
@@ -12,12 +12,13 @@ def play():
         room = world.tile_exists(player.location_x, player.location_y)
         room.modify_player(player)
         if player.is_alive() and not player.victory:
-            time.sleep(0) #changed for debugging faster
+            time.sleep(2) #changed for debugging faster
             print("\n===========================================\nChoose an action:\n")
             available_actions = room.available_actions()
             for action in available_actions:
                 print(action)
             action_input = input('Action: ')
+            print("\n\n\n\n\n\n\n")
             for action in available_actions:
                 if action_input == action.hotkey:
                     player.do_action(action, **action.kwargs)
